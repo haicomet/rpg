8f4197f2a07bf6da4938ea2c0ae9bee42e460e47
hailsom <hailiassommerville@gmail.com>
2025-04-14 03:01:28 -0400

    ideas

--- rpg/item.h
+++ rpg/item.h
@@ -4,4 +4,21 @@
 //
 //  Created by Hailia Sommerville on 4/13/25.
 //
+#ifndef ITEM_H
+#define ITEM_H
 
+#include <iostream>
+using namespace std;
+
+class Item{
+private:
+    string name,type;
+    int boost;
+public:
+    Item(string,string,int);
+    //menu of items available
+    //access granted based on level
+}
+
+
+#endif
