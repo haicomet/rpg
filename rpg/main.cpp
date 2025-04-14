22257bfcb124f2a32d7b9f8431bd90f2dd10cb3a
hailsom <hailiassommerville@gmail.com>
2025-04-13 20:47:02 -0400

    Initial Commit

---
+++ rpg/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  rpg
+//
+//  Created by Hailia Sommerville on 4/13/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
